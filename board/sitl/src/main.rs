e5527ee6c69553848b7c61a992e29a958e327462
Duc Le <minhducle.dev@gmail.com>
2025-03-04 00:30:17 +0700

    refactor: rename flight_axis module to flight_axis_helper and update references

--- board/sitl/src/main.rs
+++ board/sitl/src/main.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use autopilot::Board;
 use sitl::FlightControllerBoard;
-mod flight_axis;
+mod flight_axis_helper;
 mod sitl;
 
 enum State {
@@ -17,47 +17,51 @@ async fn main() -> anyhow::Result<()> {
     let mut last_update_time = std::time::Instant::now();
     let mut minimum_elapsed_duration = Duration::from_millis(1000); // 1 Hz
     let mut state = State::Initializing;
-    let mut flight_axis_con = Option::None;
+    let mut flight_axis = flight_axis_helper::FlightAxis::new();
     let mut update_cycle_count = 0;
     loop {
         let now = std::time::Instant::now();
         let dt = now.duration_since(last_update_time);
-        last_update_time = now;
-
         if dt < minimum_elapsed_duration {
             let remaining_duration = minimum_elapsed_duration - dt;
             tokio::time::sleep(remaining_duration).await;
             continue;
         }
 
+        last_update_time = now;
+
         update_cycle_count += 1;
 
         match state {
             State::Initializing => {
                 println!("Initializing...");
-                match flight_axis::FlightAxis::connect().await {
-                    Ok(flight_axis) => {
-                        flight_axis_con = Some(flight_axis);
-                        state = State::Running;
-                        println!("Connected to flight axis");
-                    }
-                    Err(err) => {
-                        eprintln!("Failed to connect to flight axis: {:?}", err);
-                        state = State::Stopping;
-                    }
-                }
+                state = State::Connecting;
             }
             State::Connecting => {
                 println!("Connecting...");
-                minimum_elapsed_duration = Duration::from_secs(1); // 1 Hz
+                match flight_axis.update().await {
+                    Ok(_) => {
+                        println!("Connected");
+                        state = State::Running;
+                    }
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                    }
+                }
             }
             State::Running => {
-                minimum_elapsed_duration = Duration::from_millis(1000 / 100);
-                let flight_axis_connection = flight_axis_con.as_mut().unwrap();
-                let f_state = flight_axis_connection.get_state().await?;
-                println!("State: {:?}", f_state);
-                state = State::Stopping;
-
+                minimum_elapsed_duration = Duration::from_millis(200); // 5 Hz
+                match flight_axis.update().await {
+                    Ok(_) => {
+                        println!("State: {:?}", flight_axis.state.input);
+                        let output = flight_axis.state.input;
+                        flight_axis.set_output(output);
+                    }
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                        state = State::Stopping;
+                    }
+                }
             }
             State::Stopping => {
                 println!("Stopping");
